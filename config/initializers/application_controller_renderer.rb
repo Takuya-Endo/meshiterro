cbef01a382398c7470f7ea6aaa435434b9f59fcd
Takuya-Endo <above5purple@gmail.com>
2021-02-11 08:00:55 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '861f6e86118007cbe38b80ca11ea5cb430614eefe8777a6378ba654992543413ece2b431ad1d4b30ca8cfc669c691a7d2a368ec55e6f954020bd5a8323fea813'
